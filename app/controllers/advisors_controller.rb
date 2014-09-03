051f459bb1656ba5fe325eab78942f085127d1b0
Spencer Tang <spencer.tang12@gmail.com>
2014-09-03 16:44:19 -0400

    Add Modal

--- app/controllers/advisors_controller.rb
+++ app/controllers/advisors_controller.rb
@@ -8,17 +8,6 @@ class AdvisorsController < ApplicationController
     @advisor = Advisor.new
   end
 
-  # GET /advisors/1
-  # GET /advisors/1.json
-  def show
-  end
-
-  # GET /advisors/new
-
-  # GET /advisors/1/edit
-
-  # POST /advisors
-  # POST /advisors.json
   def create
 
     @advisor = Advisor.new
@@ -31,38 +20,4 @@ class AdvisorsController < ApplicationController
       end
     end
 
-  # PATCH/PUT /advisors/1
-  # PATCH/PUT /advisors/1.json
-  def update
-    respond_to do |format|
-      if @advisor.update(advisor_params)
-        format.html { redirect_to @advisor, notice: 'Advisor was successfully updated.' }
-        format.json { render :show, status: :ok, location: @advisor }
-      else
-        format.html { render :edit }
-        format.json { render json: @advisor.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /advisors/1
-  # DELETE /advisors/1.json
-  def destroy
-    @advisor.destroy
-    respond_to do |format|
-      format.html { redirect_to advisors_url, notice: 'Advisor was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_advisor
-      @advisor = Advisor.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def advisor_params
-      params.require(:advisor).permit(:domain_name, :first_name, :last_name)
-    end
 end
