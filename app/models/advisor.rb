27330a2cc785f602f961767eee3c3c811ffbe835
Spencer Tang <spencer.tang12@gmail.com>
2014-09-03 16:25:15 -0400

    Add Predict Email Method To Advisor Model

--- app/models/advisor.rb
+++ app/models/advisor.rb
@@ -1,2 +1,32 @@
 class Advisor < ActiveRecord::Base
+
+  def self.predict_email(hash)
+
+    first_name = hash.values[0].strip.downcase
+    last_name = hash.values[1].strip.downcase
+    domain = hash.values[2].strip.downcase
+
+    if domain == "alphasights.com"
+
+      email = [first_name + "." + last_name + "@" + domain ]
+
+    elsif domain == "google.com"
+
+      option1 = first_name + "." + last_name[0] + "@" +  domain 
+
+      option2 = first_name[0] + "." + last_name + "@" +  domain 
+
+      emails = [option1, option2]
+
+    elsif domain == "apple.com"
+
+      email = [first_name[0] + "." +last_name[0] + "@" +  domain ]
+
+    else
+      []
+
+    end
+
+  end
+
 end
